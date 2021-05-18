6ca02ebfb7a65177f378c4c54368fca5007fe029
(nithinmkp) <write2nithinm@gmail.com>
2021-05-19 05:03:37 +0530

    Added a general code for multiple parameters

--- Render.R
+++ Render.R
@@ -1,6 +1,7 @@
 # Render one file
 library(glue)
 library(rlist)
+library(tidyverse)
 dir.create("Reports")
 rmarkdown::render(input = "Reporting.Rmd",
                   params = list(species="Gentoo"),
@@ -29,3 +30,15 @@ params2<-cross2(species,category) %>% map(setNames,c("species","category"))
 walk2(paths2,params2,~rmarkdown::render("Reporting.Rmd",output_file = .x,
                                       params = .y))
 
+
+#Multiple parameter- Another way (General)
+category<-data %>% select(where(is.factor)) %>% select(-1) %>% names()
+paths2<-list.expand(species,category) %>% list.stack() %>% unite(sep = "-",col = "path",remove = T) %>% 
+        map(~paste0("Reports/",.x,".pdf")) %>% flatten()
+
+params2<-list.expand(species,category) %>% map(setNames,c("species","category"))
+walk2(paths2,params2,~rmarkdown::render("Reporting.Rmd",output_file = .x,
+                                        params = .y))
+
+cbind(paths2,params2)
+pwalk("Reporting.Rmd",list(cbind(paths2,params2)))
